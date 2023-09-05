6fdb8510a2077afd96c52cdaaf6638f4c85e2ca6
Andrew Kimutai <61423343+kimutaiandy@users.noreply.github.com>
2023-09-05 12:58:15 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,14 +1,26 @@
-import socket
+import socket #here we import the socket module from the python library
 
+#creating a server socket object - specifying the socket type and family
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+#host = '192.168.56.1'
 host = socket.gethostbyname()
 port = 444
 
-serversocket.bind((host, port))
+#we bind address to port/socket
+serversocket.bind(('192.168.56.1', port)) #host will be replaced/substituted with IP, if changed and not running on host
 
+#starting tcp listener
 serversocket.listen(3)
 
 while True:
+    #starting the connection
     clientsocket, address = serversocket.accept()
+
+    print("received connection from" % str(address))
+
+    message = "hello! Thank you for connecting to the server"  "\r\n"
+    clientsocket.send(message.encode('ascii'))
+
+    clientsocket.close()
     
